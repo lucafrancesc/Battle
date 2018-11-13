62aa1927baeb234f246e15002b589c9fba92544e
Tomas Doherty <tomasdoh1@gmail.com>
2018-11-13 16:44:37 +0000

    First feature spec, added names functionality

--- app.rb
+++ app.rb
@@ -3,7 +3,14 @@ require 'shotgun'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
   end
+
+  post '/names' do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb(:play)
+  end
+  
   run! if app_file == $0
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,8 +1,17 @@
 # require_relative "./app"
 
-feature 'testing infrastructure' do
-  scenario 'Can run app and check content' do
+# feature 'testing infrastructure' do
+#   scenario 'Can run app and check content' do
+#     visit '/'
+#     expect(page).to have_content 'Testing infrastructure working!'
+#   end
+
+feature 'testing player names' do
+  scenario 'can fill in names in a form and then see names on site' do
     visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in 'player1', with: 'Luca'
+    fill_in 'player2', with: 'Tomas'
+    click_on 'Submit'
+    expect(page).to have_content 'Luca vs. Tomas'
   end
 end
