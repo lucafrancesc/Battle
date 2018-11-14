f8e5b0f4901da5a2d294910de27c46cc9a07fe4f
Luca <eto.lucafrancesco@gmail.com>
2018-11-14 14:15:00 +0000

    Second test and redirect.

--- app.rb
+++ app.rb
@@ -2,15 +2,24 @@ require 'sinatra/base'
 require 'shotgun'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
-    erb(:play)
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
   end
-  
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb :play
+  end
+
   run! if app_file == $0
 end
