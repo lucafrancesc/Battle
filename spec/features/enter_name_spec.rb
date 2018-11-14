cb203a9695b4a71f052544c61f7e4b6dd17d6700
Luca <eto.lucafrancesco@gmail.com>
2018-11-14 14:35:54 +0000

    Adding player 2 health

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -14,4 +14,12 @@ feature 'testing player names' do
     click_on 'Submit'
     expect(page).to have_content 'Luca vs. Tomas'
   end
+
+  scenario 'can fill in names in a form and then see names on site' do
+    visit '/'
+    fill_in 'player1', with: 'Luca'
+    fill_in 'player2', with: 'Tomas'
+    click_on 'Submit'
+    expect(page).to have_content 'Tomas 100 HP'
+  end
 end
